e2269770afaaef3183aeec0b36ecce6fd756b610
Alexandre <alexandrepj13@gmail.com>
2022-02-17 20:53:34 -0200

    MENU FINALIZADO

--- public/carrinho.js
+++ public/carrinho.js
@@ -1,7 +1,6 @@
+/*CHAMADA DO BANNER CARRINHO*/
+
 document.getElementById("icone-cart").addEventListener("click"
 ,()=>{
   document.querySelector(".itens-car").classList.toggle("car-div")
 })
-
-
-
--- public/index.js
+++ public/index.js
@@ -2,3 +2,9 @@ document.querySelector(".btn-form-contato")
 .addEventListener("click",()=>{
   event.preventDefault();
 })
+
+document.querySelector(".fa-bars")
+.addEventListener("click",()=>{
+  document.querySelector(".links-menu")
+  .classList.toggle("menu-animation")
+})
