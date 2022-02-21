9fbe84c779da719d026a439f5a5e4436aa76784b
Alexandre <alexandrepj13@gmail.com>
2022-02-21 20:40:52 -0300

    Finalizando css e html da page

--- public/carrinho.js
+++ public/carrinho.js
@@ -10,35 +10,28 @@ document.getElementById("icone-cart").addEventListener("click"
 const carrinho = []
 
 const BtnProductor = document.querySelectorAll(".btn-car")
-/*ADICIONANDO EVENTO NO BOTAO*/
+
 BtnProductor.forEach((element)=>{
   element.addEventListener("click",addElementCarrinho)
 })
 
-/*FUNCAO DE ADICIONAR ELEMENTO NO CARRINHO*/
-function addElementCarrinho(e){
-  /*FAZ REFERENCIA AO OBJETO*/
+function addElementCarrinho(e){  
   const button = e.target
   const item = button.closest(".card")
   const cardTitle = item.querySelector(".title-card").innerHTML;
   const precoCard = item.querySelector(".value-element").innerHTML;
   const itemImg = item.querySelector(".img-card").src;
-  
   const newItem = {
     imgcard:itemImg,
     titulocard: cardTitle,
     precocard:precoCard,
   }
-
   upgradeCar(newItem)
 }
-
 function upgradeCar(newItem){
-
   carrinho.push(newItem)
   renderCarrinho()
 }
-
 function renderCarrinho(){
   carrinho.map((itemElement)=>{
     const elemento = document.createElement("li")
