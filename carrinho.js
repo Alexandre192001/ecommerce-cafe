7bc76183ed60d5c2da89f760c0fa14a7df9a8d52
Alexandre <alexandrepj13@gmail.com>
2022-03-10 11:31:18 -0300

    Faltando finalizar carrinho de compras

--- carrinho.js
+++ carrinho.js
@@ -8,7 +8,7 @@ document.getElementById("icone-cart").addEventListener("click"
 
 
 const carrinho = []
-
+const tbody = document.querySelector(".tbody-table")
 const BtnProductor = document.querySelectorAll(".btn-car")
 
 BtnProductor.forEach((element)=>{
@@ -25,30 +25,63 @@ function addElementCarrinho(e){
     imgcard:itemImg,
     titulocard: cardTitle,
     precocard:precoCard,
+    quantidade:1,
   }
   upgradeCar(newItem)
 }
 function upgradeCar(newItem){
+    const valorItem = tbody.getElementsByClassName("valor-item")
+  for(let i=0; i<carrinho.length;i++){
+    if(carrinho[i].titulocard.trim()=== newItem.titulocard.trim()){
+      carrinho[i].quantidade++
+      const inputValor = valorItem[i]
+      inputValor.value++
+      ValorTotalCarrinho()
+      return null;
+     
+    }
+    
+  }
+  
+
   carrinho.push(newItem)
   renderCarrinho()
 }
 function renderCarrinho(){
-  carrinho.map((itemElement)=>{
-    const elemento = document.createElement("li")
-    const containerElement = `
-    <div class="card">
-    <div class="img-card-gallery">
-      <img  class="img-card" src="${itemElement.imgcard}">
-    </div>
-    <h4 class="title-card">${itemElement.titulocard}</h4>
-    <p class="value-element">${itemElement.precocard}</p>
-  </div>
+  tbody.innerHTML=""
+  carrinho.map((carrinhoElementos)=>{
+    const tr = document.createElement("tr")
+    
+    const content  =`
+    <td class="img-item">
+    <img src=${carrinhoElementos.imgcard} alt="">
+  </td>
+  <td class="title-item">${carrinhoElementos.titulocard}</td>
+  <td class="preco-item">${carrinhoElementos.precocard}</td>
+  <td class="quantidade-item">
+    <input type="number" class="valor-item" min="1" max="1" value=${carrinhoElementos.quantidade}>
+    
+  </td> 
+  <td class="remover-Item">
+    <button class="remover">X</button>
+  </td>
     `
-    elemento.innerHTML = containerElement
-    document.querySelector(".total").appendChild(elemento)
+    tr.innerHTML=content;
+    tbody.append(tr)
   })
 
+}
 
+
+function ValorTotalCarrinho(){
+  let Total = 0;
+  const ItemTotal = document.querySelector(".total-preco")
+  carrinho.forEach((item)=>{
+    const preco = Number(item.precocard.replace("$", ""))
+    Total = Total + preco*item.quantidade
+  })
+
+  ItemTotal.innerHTML = `R$ ${Total}`
 }
 
 
